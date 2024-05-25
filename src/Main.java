123320b9ef5c2b540edc4d27e01548007dbbd8b1
sahin <sahinhuseyinn0307@gmal.com>
2024-05-25 17:53:56 +0300

    Finished commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,35 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
     //Farklı metodlarla return kavramı.
+    public static int Toplama(int a, int b) {
+        return a + b;
+    }
+    public static int Carpim(int a, int b) {
+        return a*b;
+    }
+    public static int ikiCarpim(int a, int b) {
+        return (a* 2) + (b * 2);
+    }
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("İlk sayıyı giriniz: ");
+        int a = sc.nextInt();
+        System.out.print("İkinci sayıyı giriniz: ");
+        int b = sc.nextInt();
+
+        System.out.println("2 ile çarp topla metodu: "+ikiCarpim(a,b));
+        System.out.println("Carpim metodu: "+Carpim(a,b));
+        System.out.println("Toplama metodu: "+Toplama(a,b));
+        //ilk önce toplama metodu çalışacak
+        //daha sonra iki ile capım metodu
+        //en son olarak carpim metodu çalışacak
+        //RETURN KAVRAMI OLMASAYDI BU İŞLEMLERİ YAPAMAZDIK.
+        System.out.println("Bütün işlemlerin sonucu: " +
+                Carpim(ikiCarpim(Toplama(a,b),a),b));//return kavramı sayesinde iç içe method kullanmak
     }
 }
